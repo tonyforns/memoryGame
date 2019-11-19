ed6077badf97c0dcc2f04822eca6dceccdfc1a2e
unknown <iaforns@gmail.com>
2019-11-19 13:23:55 -0300

    Primer commit

---
+++ Assets/Scripts/Card/Card.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : ScriptableObject
+{
+    public int Id;
+    public Sprite Image;
+}
---
+++ Assets/Scripts/Card/CardController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    public Card cardDef;
+    public GameObject cardObject;
+    public Sprite cardImage;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateCard()
+    {
+        cardImage = cardDef.Image;
+    }
+
+    public void FlipCard()
+    {
+
+    }
+
+}
