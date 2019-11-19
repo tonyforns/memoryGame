59b177ee18caf414dd2f9e2f5011cf805a8b1c16
unknown <iaforns@gmail.com>
2019-11-19 14:09:04 -0300

    Flip card

--- Assets/Scripts/Card/CardController.cs
+++ Assets/Scripts/Card/CardController.cs
@@ -7,16 +7,23 @@ public class CardController : MonoBehaviour
     public Card cardDef;
     public GameObject cardObject;
     public Sprite cardImage;
+    public float flipVelocity = 1;
+
+    public bool flipCard;
+    public bool hideCard;
+    public bool showCard;
 
     void Start()
     {
-        
+        flipCard = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (flipCard) FlipCardUpdate();
+        if (hideCard) HideCardUpdate();
+        if (showCard) ShowCardUpdate();
     }
 
     public void UpdateCard()
@@ -26,7 +33,56 @@ public class CardController : MonoBehaviour
 
     public void FlipCard()
     {
+        flipCard = true;
+    }
 
+    public void FlipCardUpdate()
+    {
+        float initalAngle = cardObject.transform.eulerAngles.y;
+        Debug.Log(cardObject.transform.eulerAngles);
+        cardObject.transform.Rotate(new Vector3(0, 1, 0) * flipVelocity );
+        if(initalAngle < 180 && cardObject.transform.eulerAngles.y > 180)
+        {
+            flipCard = false;
+            cardObject.transform.SetPositionAndRotation(cardObject.transform.position, Quaternion.Euler(0, 180, 0));
+        }
+        if (initalAngle > 180 && cardObject.transform.eulerAngles.y < 180)
+        {
+            flipCard = false;
+            cardObject.transform.SetPositionAndRotation(cardObject.transform.position, Quaternion.Euler(0, 0, 0));
+        }
+    }
+
+    public void ShowCard()
+    {
+        showCard = true;
+    }
+
+    public void ShowCardUpdate()
+    {
+        float initalAngle = cardObject.transform.eulerAngles.y;
+        cardObject.transform.Rotate(new Vector3(0, 1, 0) * flipVelocity);
+        if (initalAngle < 180 && cardObject.transform.eulerAngles.y > 180)
+        {
+            showCard = false;
+            cardObject.transform.SetPositionAndRotation(cardObject.transform.position, Quaternion.Euler(0, 180, 0));
+        }
+    }
+
+    public void HideCard()
+    {
+        hideCard = true;
+    }
+
+    public void HideCardUpdate()
+    {
+        float initalAngle = cardObject.transform.eulerAngles.y;
+        cardObject.transform.Rotate(new Vector3(0, 1, 0) * flipVelocity);
+        if (initalAngle > 180 && cardObject.transform.eulerAngles.y < 180)
+        {
+            hideCard = false;
+            cardObject.transform.SetPositionAndRotation(cardObject.transform.position, Quaternion.Euler(0, 0, 0));
+        }
     }
 
 }
