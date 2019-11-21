423e273a85b4b3f8db78a610e4f37c0efc7f113b
unknown <iaforns@gmail.com>
2019-11-21 12:28:22 -0300

    Add Character - Chat

--- Assets/Scripts/Chat/Choise.cs
+++ Assets/Scripts/Chat/Choise.cs
@@ -2,17 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Choise : MonoBehaviour
+public class Choise : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string Chat;
+    public Chat NextChat;
 }
